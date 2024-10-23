95a6949d2e1097ae42f5cda03252b873747d65ef
Seal <gamedevseals@gmail.com>
2024-10-23 14:45:51 +0300

    School c# lab 2

---
+++ 00-School/C#/Proiect 1StudentApplication/StudentModel/User.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using UserModel;
+using System.IO;
+using System.Diagnostics.Eventing.Reader;
+using System.Diagnostics;
+
+namespace UserModel
+{
+    public class UserList //(colectie de obiecte de tip User)
+    {
+        public List<User> Users { get; set; }
+
+        public UserList()
+        {
+            Users = new List<User>();
+        }
+    }
+
+    public class User
+    {
+        public string Username { get; set; }
+        public string Email{ get; set; }
+        public string Password{ get; set; }
+        public bool LoggedIn{ get; set; }
+
+        /*
+        public override bool Equals(object obj)
+        {
+            if(obj is User) return false;
+
+            User user = obj as User;
+
+            return user.Username==Username && user.Email==Email && user.Password==Password;
+        }
+        */
+
+
+        public void RegisterAccount(UserList list)
+        {
+            Console.WriteLine("REGISTER YOUR ACCOUNT:");
+            Console.Write("Email: ");
+            Email = Console.ReadLine();
+
+            Console.Write("Password: ");
+            Password = Console.ReadLine();
+
+            Console.Write("Username: ");
+            Username = Console.ReadLine();
+
+            list.Users.Add(new User { Username = Username, Email = Email, Password = Password });
+
+            SaveToFile();
+            Console.WriteLine('\n');
+        }
+
+        public void LoadAccount()
+        {
+            Console.WriteLine("What is your Email?");
+            Email= Console.ReadLine();
+
+            //If we found it:
+            if (SearchFileForEmail(Email) != null)
+            {
+                Console.WriteLine("Email found!");
+                Console.WriteLine("...");
+                Console.WriteLine("But what's your Password?");
+                string[] info = SearchFileForEmail(Email).Split(',');
+
+                Password = Console.ReadLine();
+                while (Password != info[1])
+                {
+                    Console.WriteLine("WRONG Password. Try again.");
+                    Password= Console.ReadLine();
+                }
+
+                Console.WriteLine("Login Success!");
+                Username = info[2];
+                Console.WriteLine($"Your username is: {Username}");
+
+                LoggedIn = true;
+            }
+            else Console.WriteLine("Email not found!");
+
+            Console.WriteLine('\n');
+        }
+
+
+            //Find if Email exists:
+        public string SearchFileForEmail(string email)
+        {
+            int lineNumber=0;
+
+            foreach (var line in File.ReadLines("Users.txt"))
+            {
+                lineNumber++;
+
+                //Email found
+                if (line.Contains(Email)) return line;
+            }
+
+            //Email not found in the file.
+            return null;
+        }
+
+        public int SearchForLineInSaveFile(string email)
+        {
+            int lineNumber = 0;
+
+            foreach (var line in File.ReadLines("Users.txt"))
+            {
+                lineNumber++;
+
+                //Email found
+                if (line.Contains(Email)) return lineNumber;
+            }
+
+            //Email not found in the file.
+            return -1;
+        }
+
+        public void DisplayInfo()
+        {
+            if(LoggedIn) Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{Email},{Password},{Username}";
+        }
+
+        public void SaveToFile()
+        {
+            File.AppendAllText("Users.txt", ToString() + "\n"); //fiecare user salvat pe o linie separata
+        }
+
+        public void DeleteAccountFromFile()
+        {
+            if(LoggedIn)
+            {
+                Console.WriteLine("Are you sure you want to delete your account?");
+                Console.WriteLine("Press 'Y' for yes or 'N' for no.");
+
+                //Read keys until you press Y or N.
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true); // 'true' to not show the key pressed
+                while (keyInfo.Key != ConsoleKey.Y && keyInfo.Key != ConsoleKey.N)
+                {
+                    Console.WriteLine("Invalid key. Press 'Y' for yes or 'N' for no.");
+                    keyInfo = Console.ReadKey(true); // Read again
+                }
+
+                if (keyInfo.Key == ConsoleKey.Y)
+                {
+                    ConfirmDeleteAccount();
+                }
+                else
+                {
+                    Console.WriteLine("Account Deletion has been cancelled.");
+                }
+            }
+        }
+        void ConfirmDeleteAccount()
+        {
+            int lineIndex = SearchForLineInSaveFile(Email);
+
+            List<string> lines = File.ReadAllLines("Users.txt").ToList();
+
+            //Remove that specific line.
+            lines.RemoveAt(lineIndex);
+
+            LoggedIn = false;
+
+            //Log out. Forget all details.
+            Username = null;
+            Password = null;
+            Username = null;
+
+            Console.WriteLine("Account Deletion success.");
+        }
+    }
+}
--- 06-classy-weather/src/App.js
+++ 06-classy-weather/src/App.js
@@ -33,13 +33,15 @@ function formatDay(dateStr) {
 }
 
 class App extends React.Component {
-        constructor(props) {
-                super(props);
-                this.state = { location: 'lisbon', isLoading: false, displayLocation: '', weather: {} };
-                this.fetchWeather = this.fetchWeather.bind(this);
-        }
+        state = { location: '', isLoading: false, displayLocation: '', weather: {} };
 
-        async fetchWeather() {
+        //Arrow functions do not lose their binding.
+        fetchWeather = async () => {
+                if (this.state.location.length < 2) {
+                        this.setState({ weather: {} });
+                        return;
+                }
+
                 try {
                         this.setState({ isLoading: true });
                         // 1) Getting location (geocoding)
@@ -59,9 +61,23 @@ class App extends React.Component {
                         const weatherData = await weatherRes.json();
                         this.setState({ weather: weatherData.daily });
                 } catch (err) {
-                        console.err(err);
+                        console.error(err);
                 } finally {
                         this.setState({ isLoading: false });
+                }
+        };
+
+        setLocation = (e) => this.setState({ location: e.target.value });
+
+        componentDidMount() {
+                //   this.fetchWeather();
+                this.setState({ location: localStorage.getItem('location') || '' });
+        }
+
+        componentDidUpdate(prevProps, prevState) {
+                if (this.state.location !== prevState.location) {
+                        this.fetchWeather();
+                        localStorage.setItem('location', this.state.location);
                 }
         }
 
@@ -69,18 +85,11 @@ class App extends React.Component {
                 return (
                         <div className="app">
                                 <h1>Classy Weather</h1>
-                                <div>
-                                        <input
-                                                type="text"
-                                                placeholder="Search from location..."
-                                                onChange={(e) => this.setState({ location: e.target.value })}
-                                                value={this.state.location}
-                                        ></input>
-                                </div>
-                                <button onClick={this.fetchWeather}>Get weather</button>
+
+                                {/* <button onClick={this.fetchWeather}>Get weather</button> */}
 
                                 {this.state.isLoading && <p className="loader">Loading...</p>}
-
+                                <Input location={this.state.location} onChangeLocation={this.setLocation} />
                                 {this.state.weather.weathercode && <Weather weather={this.state.weather} location={this.state.displayLocation} />}
                         </div>
                 );
@@ -88,6 +97,21 @@ class App extends React.Component {
 }
 
 export default App;
+
+class Input extends React.Component {
+        render() {
+                return (
+                        <div>
+                                <input
+                                        type="text"
+                                        placeholder="Search from location..."
+                                        value={this.props.location}
+                                        onChange={this.props.onChangeLocation}
+                                ></input>
+                        </div>
+                );
+        }
+}
 
 class Weather extends React.Component {
         render() {
@@ -98,7 +122,7 @@ class Weather extends React.Component {
                                 <h2>Weather {this.props.location}</h2>
                                 <ul className="weather">
                                         {dates.map((date, i) => (
-                                                <Day date={date} max={max[i]} min={min[i]} code={codes[i]} key={date} />
+                                                <Day date={date} max={max[i]} min={min[i]} code={codes[i]} key={date} isToday={i === 0} />
                                         ))}
                                 </ul>
                         </div>
@@ -108,12 +132,12 @@ class Weather extends React.Component {
 
 class Day extends React.Component {
         render() {
-                const { date, max, min, code } = this.props;
+                const { date, max, min, code, isToday } = this.props;
 
                 return (
                         <li className="day">
                                 <span>{getWeatherIcon(code)}</span>
-                                <p>{formatDay(date)}</p>
+                                <p>{isToday ? <strong>Today</strong> : formatDay(date)}</p>
                                 <p>
                                         {Math.floor(min)}&deg; &mdash; <strong>{Math.ceil(max)}&deg;</strong>
                                 </p>
