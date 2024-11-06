e6518da1d30fcd8b7513e97bc5764302298cbdcc
Seal <gamedevseals@gmail.com>
2024-11-06 20:00:15 +0200

    Old Redux

--- 11-redux-intro/src/features/accounts/AccountOperations.js
+++ 11-redux-intro/src/features/accounts/AccountOperations.js
@@ -1,78 +1,89 @@
-import { useState } from "react";
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { deposit, payLoan, requestLoan, withdraw } from './accountSlice';
 
 function AccountOperations() {
-  const [depositAmount, setDepositAmount] = useState("");
-  const [withdrawalAmount, setWithdrawalAmount] = useState("");
-  const [loanAmount, setLoanAmount] = useState("");
-  const [loanPurpose, setLoanPurpose] = useState("");
-  const [currency, setCurrency] = useState("USD");
+        const [depositAmount, setDepositAmount] = useState('');
+        const [withdrawalAmount, setWithdrawalAmount] = useState('');
+        const [loanAmount, setLoanAmount] = useState('');
+        const [loanPurpose, setLoanPurpose] = useState('');
+        const [currency, setCurrency] = useState('USD');
 
-  function handleDeposit() {}
+        const dispatch = useDispatch();
+        const { balance, loan: currentLoan, loanPurpose: currentLoanPurpose, isLoading } = useSelector((store) => store.account);
+        console.log(balance);
 
-  function handleWithdrawal() {}
+        function handleDeposit() {
+                if (!depositAmount) return;
 
-  function handleRequestLoan() {}
+                dispatch(deposit(depositAmount, currency));
+                setDepositAmount('');
+                setCurrency('USD');
+        }
 
-  function handlePayLoan() {}
+        function handleWithdrawal() {
+                if (!withdrawalAmount) return;
+                if (balance < withdrawalAmount) return;
 
-  return (
-    <div>
-      <h2>Your account operations</h2>
-      <div className="inputs">
-        <div>
-          <label>Deposit</label>
-          <input
-            type="number"
-            value={depositAmount}
-            onChange={(e) => setDepositAmount(+e.target.value)}
-          />
-          <select
-            value={currency}
-            onChange={(e) => setCurrency(e.target.value)}
-          >
-            <option value="USD">US Dollar</option>
-            <option value="EUR">Euro</option>
-            <option value="GBP">British Pound</option>
-          </select>
+                dispatch(withdraw(withdrawalAmount));
+                setWithdrawalAmount('');
+        }
 
-          <button onClick={handleDeposit}>Deposit {depositAmount}</button>
-        </div>
+        function handleRequestLoan() {
+                if (!loanAmount || !loanPurpose) return;
+                if (currentLoan > 0) return;
 
-        <div>
-          <label>Withdraw</label>
-          <input
-            type="number"
-            value={withdrawalAmount}
-            onChange={(e) => setWithdrawalAmount(+e.target.value)}
-          />
-          <button onClick={handleWithdrawal}>
-            Withdraw {withdrawalAmount}
-          </button>
-        </div>
+                setLoanAmount('');
+                setLoanPurpose('');
+                dispatch(requestLoan(loanAmount, loanPurpose));
+        }
 
-        <div>
-          <label>Request loan</label>
-          <input
-            type="number"
-            value={loanAmount}
-            onChange={(e) => setLoanAmount(+e.target.value)}
-            placeholder="Loan amount"
-          />
-          <input
-            value={loanPurpose}
-            onChange={(e) => setLoanPurpose(e.target.value)}
-            placeholder="Loan purpose"
-          />
-          <button onClick={handleRequestLoan}>Request loan</button>
-        </div>
+        function handlePayLoan() {
+                dispatch(payLoan());
+        }
 
-        <div>
-          <span>Pay back $X</span>
-          <button onClick={handlePayLoan}>Pay loan</button>
-        </div>
-      </div>
-    </div>
-  );
+        return (
+                <div>
+                        <h2>Your account operations</h2>
+                        <div className="inputs">
+                                <div>
+                                        <label>Deposit</label>
+                                        <input type="number" value={depositAmount} onChange={(e) => setDepositAmount(+e.target.value)} />
+                                        <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
+                                                <option value="USD">US Dollar</option>
+                                                <option value="EUR">Euro</option>
+                                                <option value="GBP">British Pound</option>
+                                        </select>
+
+                                        <button onClick={handleDeposit} disabled={isLoading}>
+                                                {isLoading ? 'Converting...' : `Deposit ${depositAmount}`}
+                                        </button>
+                                </div>
+
+                                <div>
+                                        <label>Withdraw</label>
+                                        <input type="number" value={withdrawalAmount} onChange={(e) => setWithdrawalAmount(+e.target.value)} />
+                                        <button onClick={handleWithdrawal}>Withdraw {withdrawalAmount}</button>
+                                </div>
+
+                                <div>
+                                        <label>Request loan</label>
+                                        <input type="number" value={loanAmount} onChange={(e) => setLoanAmount(+e.target.value)} placeholder="Loan amount" />
+                                        <input value={loanPurpose} onChange={(e) => setLoanPurpose(e.target.value)} placeholder="Loan purpose" />
+                                        <button onClick={handleRequestLoan}>Request loan</button>
+                                </div>
+
+                                {currentLoan > 0 && (
+                                        <div>
+                                                <span>
+                                                        Pay back ${currentLoan}({currentLoanPurpose})
+                                                </span>
+                                                <button onClick={handlePayLoan}>Pay loan</button>
+                                        </div>
+                                )}
+                        </div>
+                </div>
+        );
 }
 
 export default AccountOperations;
--- 11-redux-intro/src/features/accounts/BalanceDisplay.js
+++ 11-redux-intro/src/features/accounts/BalanceDisplay.js
@@ -1,12 +1,21 @@
+import { connect } from 'react-redux';
+import thunk from 'redux-thunk';
+
 function formatCurrency(value) {
-  return new Intl.NumberFormat("en", {
-    style: "currency",
-    currency: "USD",
-  }).format(value);
+        return new Intl.NumberFormat('en', {
+                style: 'currency',
+                currency: 'USD',
+        }).format(value);
 }
 
-function BalanceDisplay() {
-  return <div className="balance">{formatCurrency(123456)}</div>;
+function BalanceDisplay({ balance }) {
+        return <div className="balance">{formatCurrency(balance)}</div>;
 }
 
-export default BalanceDisplay;
+function mapStateToProps(state) {
+        return {
+                balance: state.account.balance,
+        };
+}
+
+export default connect(mapStateToProps)(BalanceDisplay);
