45d44f523aed8ca9d23677a088ffea209899094d
Seal <gamedevseals@gmail.com>
2024-11-07 07:33:20 +0200

    accountSlice convert to modern redux

--- 11-redux-intro/src/features/accounts/accountSlice.js
+++ 11-redux-intro/src/features/accounts/accountSlice.js
@@ -1,11 +1,71 @@
-const initialStateAccount = {
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
         balance: 0,
         loan: 0,
         loanPurpose: '',
         isLoading: false,
 };
 
-export default function accountReducer(state = initialStateAccount, action) {
+const accountSlice = createSlice({
+        name: 'account',
+        initialState,
+        reducers: {
+                deposit(state, action) {
+                        state.balance += action.payload;
+                        state.isLoading = false;
+                },
+                withdraw(state, action) {
+                        state.balance -= action.payload;
+                },
+                requestLoan: {
+                        prepare(amount, purpose) {
+                                return {
+                                        payload: { amount, purpose },
+                                };
+                        },
+                        reducer(state, action) {
+                                if (state.loan > 0) return;
+
+                                state.balance += action.payload.amount;
+                                state.loan += action.payload.amount;
+                                state.loanPurpose += action.payload.purpose;
+                        },
+                },
+                payLoan(state) {
+                        state.balance -= state.loan;
+                        state.loan = 0;
+                        state.loanPurpose = '';
+                },
+                convertingCurrency(state) {
+                        state.isLoading = true;
+                },
+        },
+});
+
+export const { withdraw, requestLoan, payLoan } = accountSlice.actions;
+
+export function deposit(amount, currency) {
+        if (currency === 'USD') return { type: 'account/deposit', payload: amount };
+
+        //if we return a function, redux automatically knows it is a "thunk"
+        return async function (dispatch, getState) {
+                dispatch({ type: 'account/convertingCurrency' });
+
+                const res = await fetch(`https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`);
+                const data = await res.json();
+                const converted = data.rates.USD;
+
+                dispatch({ type: 'account/deposit', payload: converted });
+        };
+}
+
+export default accountSlice.reducer;
+// console.log(accountSlice);
+// console.log(requestLoan(1000, 'wow'));
+
+/*
+export default function accountReducer(state = initialState, action) {
         switch (action.type) {
                 case 'account/deposit':
                         return { ...state, balance: state.balance + action.payload, isLoading: false };
@@ -50,3 +110,4 @@ export function requestLoan(amount, purpose) {
 export function payLoan() {
         return { type: 'account/payLoan' };
 }
+*/
