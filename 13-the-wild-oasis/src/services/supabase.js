d7dc6648a0e6c50fc41398d948ad262aa45bf667
Seal <gamedevseals@gmail.com>
2024-11-16 14:13:31 +0200

    React query

---
+++ 13-the-wild-oasis/src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+const supabaseUrl = 'https://fyijirqbidoxyhkevtgh.supabase.co';
+const supabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ5aWppcnFiaWRveHloa2V2dGdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzE3NDIxMjksImV4cCI6MjA0NzMxODEyOX0.glJszMpDIMlCImFOyzOjXtFz8b16_pECQzTrx45ehf0';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
