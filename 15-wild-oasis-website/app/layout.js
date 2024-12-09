0ea17f6f638cfa27020693096e1e2669493c9d1f
Seal <gamedevseals@gmail.com>
2024-12-09 12:30:33 +0200

    Loading indicator

--- 15-wild-oasis-website/app/layout.js
+++ 15-wild-oasis-website/app/layout.js
@@ -1,6 +1,13 @@
 import Logo from '@/app/_components/Logo';
 import Navigation from '@/app/_components/Navigation';
 import '@/app/_styles/globals.css';
+import { Josefin_Sans } from 'next/font/google';
+import Header from './_components/Header';
+
+const josefin = Josefin_Sans({
+      subsets: ['latin'],
+      display: 'swap',
+});
 
 export const metadata = {
       // title: 'The Wild Oasis',
@@ -14,12 +21,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
       return (
             <html lang="en">
-                  <body className="bg-primary-950 text-primary-100 min-h-screen">
-                        <header>
-                              <Logo />
-                        </header>
-                        <Navigation />
-                        <main>{children}</main>
+                  <body className={`${josefin.className} antialiased bg-primary-950 flex flex-col text-primary-100 min-h-screen`}>
+                        <Header />
+
+                        <div className="flex-1 px-8 py-12 grid">
+                              <main className="max-w-7xl mx-auto w-full">{children}</main>
+                        </div>
                   </body>
             </html>
       );
