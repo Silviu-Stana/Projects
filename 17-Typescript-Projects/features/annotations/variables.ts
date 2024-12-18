d634119e23292c223607661d8cbfc08b0120f3a6
Seal <gamedevseals@gmail.com>
2024-12-18 08:51:12 +0200

    Learning basic types of Typescript

---
+++ 17-Typescript-Projects/features/annotations/variables.ts
@@ -0,0 +1,21 @@
+let apples: number = 5;
+let speed: string = 'fast';
+let hasName: boolean = true;
+let nothingMuch: null = null;
+let nothing: undefined = undefined;
+
+let now: Date = new Date();
+
+let colors: string[] = ['red', 'green', 'blue'];
+let myNumbers: number[] = [1];
+let truths: boolean[] = [true, false];
+
+class Car {}
+
+let car: Car = new Car();
+
+//Object Literal
+let point: { x: number; y: number } = {
+      x: 10,
+      y: 20,
+};
