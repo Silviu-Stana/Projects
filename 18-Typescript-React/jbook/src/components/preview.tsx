b70ffdd83f0f6fc817737c013954d3db74ab1b3b
Seal <gamedevseals@gmail.com>
2025-01-26 19:32:24 +0200

    Finish code block and now begin markdown

--- 18-Typescript-React/jbook/src/components/preview.tsx
+++ 18-Typescript-React/jbook/src/components/preview.tsx
@@ -1,38 +1,56 @@
 import { useRef, useEffect } from 'react';
+import './preview.css';
 
 interface PreviewProps {
       code: string;
+      err: string;
 }
 
 const html = `
     <html>
-      <head></head>
+      <head>
+            <style>html { background-color: white; } </style>
+      </head>
       <body>
         <div id="root"></div>
         <script>
+            const handleError = (err) => {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);      
+            };
+            
+            window.addEventListener('error', ()=> {
+                    event.preventDefault();
+                    handleError(event.error);
+            });
+        
           window.addEventListener('message', (event) => {
             try {
               eval(event.data);
-            } catch (err) {
-              const root = document.querySelector('#root');
-              root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>';
-              console.error(err);
-            }
+            } catch (err) { handleError(err) }
           }, false);
         </script>
       </body>
     </html>
   `;
 
-const Preview: React.FC<PreviewProps> = ({ code }) => {
+const Preview: React.FC<PreviewProps> = ({ code, err }) => {
       const iframe = useRef<any>();
 
       useEffect(() => {
             iframe.current.srcdoc = html;
-            iframe.current.contentWindow.postMessage(code, '*');
+            iframe.current.onload = () => {
+                  iframe.current.contentWindow.postMessage(code, '*');
+            };
       }, [code]);
 
-      return <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />;
+      return (
+            <div className="preview-wrapper">
+                  <iframe title="preview" ref={iframe} sandbox="allow-scripts" srcDoc={html} />
+                  {err && <div className="preview-error">{err}</div>}
+            </div>
+      );
 };
 
 export default Preview;
