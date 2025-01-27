929df9ed218fc795823062f6ee00ba6ff0482ef0
Seal <gamedevseals@gmail.com>
2025-01-27 21:16:43 +0200

    Markdown editor

--- 18-Typescript-React/jbook/src/components/text-editor.tsx
+++ 18-Typescript-React/jbook/src/components/text-editor.tsx
@@ -5,6 +5,7 @@ import './text-editor.css';
 const TextEditor: React.FC = () => {
       const [editing, setEditing] = useState(false);
       const ref = useRef<HTMLDivElement | null>(null);
+      const [value, setValue] = useState('# Header');
 
       useEffect(() => {
             const listener = (event: MouseEvent) => {
@@ -20,15 +21,16 @@ const TextEditor: React.FC = () => {
 
       if (editing) {
             return (
-                  <div ref={ref}>
-                        <MDEditor />
+                  <div className="text-editor" ref={ref}>
+                        <MDEditor value={value} onChange={(v) => setValue(v || '')} />
                   </div>
             );
       }
 
       return (
-            <div onClick={() => setEditing(true)}>
-                  <MDEditor.Markdown source={'# Header'} />
+            <div className="text-editor card" onClick={() => setEditing(true)}>
+                  <div className="card-content"></div>
+                  <MDEditor.Markdown source={value} />
             </div>
       );
 };
