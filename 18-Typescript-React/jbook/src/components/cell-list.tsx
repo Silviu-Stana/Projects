86177ac49b3b4dc51a74c219317d605056a7b58e
Seal <gamedevseals@gmail.com>
2025-02-11 20:31:08 +0200

    "Add text cell" Buttons - between all cells

--- 18-Typescript-React/jbook/src/components/cell-list.tsx
+++ 18-Typescript-React/jbook/src/components/cell-list.tsx
@@ -1,4 +1,6 @@
+import { Fragment } from 'react';
 import { useTypedSelector } from '../hooks/use-typed-selectors';
+import AddCell from './add-cell';
 import CellListItem from './cell-list-item';
 
 const CellList: React.FC = () => {
@@ -8,9 +10,19 @@ const CellList: React.FC = () => {
             });
       });
 
-      const renderedCells = cells.map((cell) => <CellListItem key={cell.id} cell={cell} />);
+      const renderedCells = cells.map((cell) => (
+            <Fragment key={cell.id}>
+                  <AddCell nextCellId={cell.id} />
+                  <CellListItem cell={cell} />
+            </Fragment>
+      ));
 
-      return <div>{renderedCells}</div>;
+      return (
+            <div>
+                  {renderedCells}
+                  <AddCell nextCellId={null} />
+            </div>
+      );
 };
 
 export default CellList;
--- 18-Typescript-React/jbook/src/index.tsx
+++ 18-Typescript-React/jbook/src/index.tsx
@@ -1,9 +1,9 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { store } from './state';
 import CellList from './components/cell-list';
+import { createRoot } from 'react-dom/client';
 
 const App = () => {
       return (
@@ -15,4 +15,6 @@ const App = () => {
       );
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const container = document.querySelector('#root');
+const root = createRoot(container!);
+root.render(<App />);
