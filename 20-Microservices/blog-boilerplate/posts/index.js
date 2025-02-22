17e0266a9d499a947f78d6cc2b97bfc85f11f8bf
Seal <gamedevseals@gmail.com>
2025-02-22 11:00:00 +0200

    nginx Ingress and Load Balancer

--- 20-Microservices/blog-boilerplate/posts/index.js
+++ 20-Microservices/blog-boilerplate/posts/index.js
@@ -11,11 +11,12 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (req, res) => {
-    res.send(posts);
-});
+//Unused: this endpoint is just for testing
+// app.get('/posts', (req, res) => {
+//     res.send(posts);
+// });
 
-app.post('/posts', async (req, res) => {
+app.post('/posts/create', async (req, res) => {
     const id = randomBytes(4).toString('hex');
     const { title } = req.body;
 
@@ -24,7 +25,7 @@ app.post('/posts', async (req, res) => {
         title,
     };
 
-    await axios.post('http://localhost:4005/events', {
+    await axios.post('http://event-bus-srv:4005/events', {
         type: 'PostCreated',
         data: { id, title },
     });
--- 20-Microservices/blog-boilerplate/query/index.js
+++ 20-Microservices/blog-boilerplate/query/index.js
@@ -45,10 +45,10 @@ app.post('/events', (req, res) => {
 app.listen(4002, async () => {
     console.log('Listen on 4002');
     try {
-        const res = await axios.get('http://localhost:4005/events');
+        const res = await axios.get('http://event-bus-srv:4005/events');
 
         for (let event of res.data) {
-            console.log('Processing event: ', event.type);
+            console.log('Received event: ', event.type);
             handleEvent(event.type, event.data);
         }
     } catch (err) {
